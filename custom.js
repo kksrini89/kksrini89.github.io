a40e573483ffe3be4dcb81501a51ae7c378610b3
Srinivasan <kksrini89@gmail.com>
2016-12-08 05:30:49 +0530

    Refactored the cluttered code

--- custom.js
+++ custom.js
@@ -2,30 +2,36 @@
  * Created by srini on 31-03-2016.
  */
 
-$(document).ready(function () {
-    $(".nav a").on('click', function () {
+$(document).ready(function() {
+    $(".nav a").on('click', function() {
         $(".nav").find('.active').removeClass("active");
         $(this).parent().addClass("active");
     });
 
-    $("#portfolio-first").hover(function () {
-        $("#overlayIconOne").addClass("fa fa-fire");
-    }, function () {
-        $("#overlayIconOne").removeClass("fa fa-fire");
-    });
-    $("#portfolio-second").hover(function () {
-        $("#overlayIconTwo").addClass("fa fa-fire");
-    }, function () {
-        $("#overlayIconTwo").removeClass("fa fa-fire");
-    });
-    $("#portfolio-third").hover(function () {
-        $("#overlayIconThree").addClass("fa fa-fire");
-    }, function () {
-        $("#overlayIconThree").removeClass("fa fa-fire");
+    $("#portfolio-container a").hover(function() {
+        $(this).find('span#overlayIcon').addClass("fa fa-fire");
+    }, function() {
+        $(this).find('span#overlayIcon').removeClass("fa fa-fire");
     });
 
+    /* $("#portfolio-first").hover(function() {
+         $("#overlayIconOne").addClass("fa fa-fire");
+     }, function() {
+         $("#overlayIconOne").removeClass("fa fa-fire");
+     });
+     $("#portfolio-second").hover(function() {
+         $("#overlayIconTwo").addClass("fa fa-fire");
+     }, function() {
+         $("#overlayIconTwo").removeClass("fa fa-fire");
+     });
+     $("#portfolio-third").hover(function() {
+         $("#overlayIconThree").addClass("fa fa-fire");
+     }, function() {
+         $("#overlayIconThree").removeClass("fa fa-fire");
+     });*/
+
     // Contact US
-    $("#submitId").on('click', function () {
+    $("#submitId").on('click', function() {
         var Name = $("#nameText").val();
         var subject = $("#subject").val();
         var msg = $("#msgBody").val() + "%0A%0A" + "-----" + "%0A" + Name;
